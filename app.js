9dc69350474e3d7a0c4ed0f91655dd30cb8e477e
Kishan Pareek <kishan25pareek@gmail.com>
2023-04-06 21:58:39 +0530

    Updated the app.listen() function to listen on the port number specified by your cloud provider.

--- app.js
+++ app.js
@@ -164,7 +164,8 @@ app.get("/:", function(req,res){
 app.get("/about", function(req, res){
   res.render("about");
 });
+const port = process.env.PORT || 3000;
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log(`Server started on port ${port}`);
 });
